616bcd7b1942c05ddfdd4a8b0d91973da0356514
Rebecca Turner <turner@mikomi.org>
2012-07-09 01:20:31 -0400

    Support use syntax qw(corolocal)

---
+++ lib/Syntax/Feature/Corolocal.pm
@@ -0,0 +1,35 @@
+# ABSTRACT: Corolocal for Syntax::Feature
+package Syntax::Feature::Corolocal;
+use common::sense;
+use Coro::Localize ();
+
+sub install {
+    my $class = shift;
+    my %args = @_;
+    Coro::Localize->import_into( $args{'into'} );
+}
+
+1;
+
+=head1 SYNOPSIS
+
+    use syntax qw( corolocal );
+    
+    async {    
+        corolocal $/ = \2_048;
+        while (<STDIN>) {
+            # ...
+        }
+    }
+
+=head1 DESCRIPTION
+
+This allows you to load L<Coro::Localize> using L<Syntax::Feature>. 
+L<Syntax::Feature> provides a single point of entry for loading various
+syntax extensions.
+
+=head1 SEE ALSO
+
+Coro::Localize
+Syntax::Feature
+
